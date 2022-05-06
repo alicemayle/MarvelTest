ec1559f45e20168d65298d551f1437cfae3b2699
alicemayle <alicemr273@gmail.com>
2022-05-05 19:25:34 -0500

    feature: utils

---
+++ marvel/src/main/java/test/albo/mx/marvel/util/Constants.java
@@ -0,0 +1,11 @@
+package test.albo.mx.marvel.util;
+
+public final class Constants {
+
+	public static final Integer LIMIT = 100;
+	
+	public static final String CREATOR_EDITOR = "editor";
+	public static final String CREATOR_WRITER = "writer";
+	public static final String CREATOR_COLORIST = "colorist";
+	
+}
---
+++ marvel/src/main/java/test/albo/mx/marvel/util/InternalException.java
@@ -0,0 +1,13 @@
+package test.albo.mx.marvel.util;
+
+public class InternalException extends Exception {
+
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = -5686218281338184869L;
+
+	public InternalException(String mensaje){
+        super(mensaje);
+    }
+}
